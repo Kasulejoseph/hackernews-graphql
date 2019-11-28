a51d29ba7858b6c50a99faadabfe43dcfe7f82bc
kasule joseph <joseph.kasule@andela.com>
2019-11-28 19:47:46 +0200

    add resolver functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import { GraphQLServer } from 'graphql-yoga'
+
+
+let link = [{
+    id: 'link-0',
+    description: 'Fullstack tutorial for GraphQL ',
+    url: 'www.howtographql.com'
+}]
+// defines graphQL schema
+const typeDefs = `
+type Query {
+    info: String!
+    feeds: [Link!]!
+}
+
+type Link {
+    id: ID!
+    description: String!
+    url: String!
+}
+`
+const resolvers = {
+    Query: {
+        info: () => 'Thats true',
+        feeds: () => link
+    },
+    Link: {
+        id: (parent) => parent.id,
+        description: (parent) => parent.description,
+        url: (parent) => parent.url
+
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => console.log(`Server Running`))
